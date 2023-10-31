7efc15338bc8ea1c6eb5da252a9302645b72d781
Joseph <JosephSped23@gmail.com>
2023-10-31 10:33:35 -0400

    completes control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let response
+  if (someValue < 400) {
+    return response = 'This one is on me!';
+  }
+  if (someValue < 2000){
+      return response = 'That will be twenty bucks.';
+  }
+  if (someValue <= 2500){
+    return response = 'I will gladly take your thirty bucks.';
+}
+  else if (someValue > 2500){
+    return response = 'No can do.';
+  } 
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+  return city === "NYC" ? "Ok, sounds good." : "No go.";
+  }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+function switchOnCharmFromTip(tipAmount){
+  switch (true) {
+    case tipAmount === 'generous':
+      return "Thank you so much.";
+    case tipAmount === 'not as generous':
+      return "Thank you.";
+    default:
+      return "Bye.";
+      
+
+  }
+
 }
